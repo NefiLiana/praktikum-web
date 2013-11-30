bbae01ede4b2b5612266c040141b5c2fea7620ae
Nefi Liana <nefiliana03@gmail.com>
2013-11-30 19:34:59 +0700

    modul 5

---
+++ 110533406981/Praktikum ke-5/test_koneksi.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 4.0 Transitional//EN" 
+<html> 
+<head>
+<title>Test Koneksi</title>
+</head>
+	<body>
+		<?php
+			require_once './koneksi.php';
+		?>
+<h4>Koneksi Berhasil</h4>
+</body>
+</html>
